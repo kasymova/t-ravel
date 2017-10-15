19b8a0681615783bea6ea07d0d7a3b83debfaafd
kasymova <raisa.kasymova@yandex.ru>
2017-10-15 08:11:44 +0300

    first commit

---
+++ t-ravel/Profile.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.swift
+//  t-ravel
+//
+//  Created by Раиса Касымова on 15.10.17.
+//  Copyright © 2017 Раиса Касымова. All rights reserved.
+//
+
+import Foundation
+
+class Profile {
+
+}
